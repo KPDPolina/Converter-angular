da386c36b5f621aa098e2f548cad9f67dc5c741d
Polina Krasnova <krasnovapolina7@gmail.com>
2023-06-21 15:50:21 +0300

    add reactive forms
    [(ngModel)] was change on reactive forms.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { ApiService } from './api.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 function foundIndex(arrayOfArrays:Array<Array<string | number>>, targetValue: number) {
     let fIndex = -1;
@@ -29,60 +30,91 @@ function foundIndex(arrayOfArrays:Array<Array<string | number>>, targetValue: nu
 
 export class AppComponent implements OnInit { 
 
-    select1: number = 1;
-    select2: number = 1;
-    input1: number = 10;
-    input2: number;
+    myForm: FormGroup;
+
+    r1: Array<string | number>;
+    r2: Array<string | number>;
+
     rates: Array<Array<string | number>> = [["UAH", 1]];
     previousRate: Array<string | number> = this.rates[0]; 
-
-    constructor(private apiService: ApiService) {}
     
+
     ngOnInit () {
         this.getDataFromApi();
     }
 
     getDataFromApi(): void {
         this.apiService.getData().subscribe(
-          (response) => {
-            console.log("response", response)
-            response.forEach(element => {
-                const currency = [element.cc, element.rate];
-                this.rates.push(currency)                
-            });
-            console.log("rates", this.rates)
-
-          },
-          (error) => {
-            console.error('An error occurred:', error);
-          }
+            (response) => {
+                this.r1 = [response[24].cc, response[24].rate];
+                this.r2 = [response[31].cc, response[31].rate];
+                response.forEach(element => {
+                    const currency = [element.cc, element.rate];
+                    this.rates.push(currency)                
+                });
+            },
+            (error) => {
+                console.error('An error occurred:', error);
+            }
         );
-      }
+    }
 
 
-    newPreviousRate(value:number): void {
+    constructor(private apiService: ApiService, private formBuilder: FormBuilder) {
+        this.myForm = this.formBuilder.group({
+          select1: 1,
+          select2: 1,
+          input1: 1,
+          input2: 1
+        });
+
+        this.myForm.controls.select1.valueChanges.subscribe(() => {
+            this.updatePreviousRate(this.myForm.controls.select1.value);
+            this.updateInput2Value();
+        });
+    
+        this.myForm.controls.select2.valueChanges.subscribe(() => {
+            this.updatePreviousRate(this.myForm.controls.select2.value);
+            this.updateInput1Value();
+        });
+    
+        this.myForm.controls.input1.valueChanges.subscribe(() => {
+            this.updateInput2Value();
+        });
+    
+        this.myForm.controls.input2.valueChanges.subscribe(() => {
+            this.updateInput1Value();
+        });
+    }
+
+    updateInput2Value(): void {
+        const input1Value: number = this.myForm.controls.input1.value;
+        const select1Value: number = this.myForm.controls.select1.value;
+        const select2Value: number = this.myForm.controls.select2.value;
+    
+        if (input1Value && select1Value && select2Value) {
+            const calculatedValue: number = parseFloat(((input1Value * select1Value) / select2Value).toFixed(4));
+            this.myForm.controls.input2.setValue(calculatedValue);} 
+            else {
+            this.myForm.controls.input2.setValue(null);
+        }
+    }
+    
+    updateInput1Value(): void {
+        const input2Value: number = this.myForm.controls.input2.value;
+        const select1Value: number = this.myForm.controls.select1.value;
+        const select2Value: number = this.myForm.controls.select2.value;
+    
+        if (input2Value && select1Value && select2Value) {
+            const calculatedValue: number = parseFloat(((input2Value * select2Value) / select1Value).toFixed(4));
+            this.myForm.controls.input1.setValue(calculatedValue);}
+            else {
+            this.myForm.controls.input1.setValue(null);
+        }
+    }
+    
+    updatePreviousRate(value:number): void {
         let i = foundIndex(this.rates, value);
         this.previousRate = this.rates[i];
     }
-
-    onSelected1(value:number): void {
-        this.select1 = value;
-        this.newPreviousRate(value)
-        this.updateInput2()
-    }
-    onSelected2(value:number): void {
-        this.select2 = value;
-        this.newPreviousRate(value)
-        this.updateInput1()
-    }
-    updateInput2() {
-        this.input2 = parseFloat(((this.input1 * this.select1) / this.select2).toFixed(4));
-    }
-    updateInput1() {
-        this.input1 = parseFloat(((this.input2 * this.select2) / this.select1).toFixed(4));
-    }
-
 }
-
-
-
